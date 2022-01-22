270baf8b8fe6c71ddc7ab42c8a09dc58e0314454
michelleroos <hello.michelleroos@gmail.com>
2022-01-22 13:15:08 -0500

    lc 209

--- min-subarr-sum.js
+++ min-subarr-sum.js
@@ -30,15 +30,19 @@ var minSubArrayLen = function (target, nums) {
   let sum = 0; 
   let left = 0;
   for (let right = 0; right < nums.length; right++) {
-    sum += nums[right];
+    sum += nums[right]; // pop()
     while (sum >= target) {
       res = Math.min(res, (right - left) + 1); // account for off-by-one
-      sum -= nums[left];
+      sum -= nums[left]; // shift()
       left++;
     };
   };
   return res === Infinity ? 0 : res;
 };
+
+console.log(minSubArrayLen(7, [2, 3, 1, 2, 4, 3])) // 2
+console.log(minSubArrayLen(4, [1, 4, 4])); // 1
+console.log(minSubArrayLen(11, [1, 1, 1, 1, 1, 1, 1, 1])) // 0
  
 /*
 Follow up: If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log(n)).
